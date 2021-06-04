6c0428a97ec8a617314d97d4415d0dc2a36f79c9
camelwater <32146663+camelwater@users.noreply.github.com>
2021-06-04 02:48:56 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -12,13 +12,15 @@ import sys
 
 sys.path.append('C:\\Users\\ryanz\\Anaconda3\\Lib\\site-packages')
 
+load_dotenv()
+KEY = os.getenv('KEY')
+SERVER_ID = 775253594848886785
+load_ext = ['cogs.table_cog']
+bot = commands.Bot(command_prefix = ('?'), case_insensitive=True, intents = discord.Intents.all(), help_command = None)      
+        
 if __name__ == "__main__":
-    load_dotenv()
-    KEY = os.getenv('KEY')
-    SERVER_ID = 775253594848886785
-    bot = commands.Bot(command_prefix = ('?'), case_insensitive=True, intents = discord.Intents.all(), help_command = None)
     
-    load_extensions = ['cogs.table_cog']
-    for l in load_extensions:
-        bot.load_extension(l)
+    for l in load_ext:
+        bot.load_extension(l)  
+        
     bot.run(KEY)
