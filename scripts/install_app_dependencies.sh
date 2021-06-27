97a4edcf43a9db0ee62917161a0801f4d225d163
camelwater <iamaguy9999@gmail.com>
2021-06-26 21:03:06 -0500

    try fix requirements

--- scripts/install_app_dependencies.sh
+++ scripts/install_app_dependencies.sh
@@ -3,4 +3,4 @@ sudo pip3 install virtualenv
 cd /home/ec2-user/app
 virtualenv environment
 source environment/bin/activate
-sudo pip3 install -r requirements.txt
+sudo pip3 install -r scripts/requirements.txt
