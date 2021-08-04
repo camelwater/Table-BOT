4eb98cf702e9e8435085c258442b7267f9b01605
camelwater <iamaguy9999@gmail.com>
2021-08-04 02:45:55 -0500

    oops im dumb

--- scripts/install_bot_dependencies.sh
+++ scripts/install_bot_dependencies.sh
@@ -1,7 +1,7 @@
 #!/bin/bash 
 sudo pip3 install virtualenv
 cd /home/ec2-user/app
-key=$(jq .BOT_KEY creds/creds.json) #retrieve key
+key=$(jq .BOT_KEY ../creds/creds.json) #retrieve key
 virtualenv environment
 source environment/bin/activate
 echo KEY=$key > .env #inject key
