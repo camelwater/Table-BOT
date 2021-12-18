9bd50d8db3fd0ce864fb401dab20eb5a9c6564f4
Ryan Zhao <32146663+camelwater@users.noreply.github.com>
2021-12-18 01:58:20 -0600

    Update install_bot_dependencies.sh

--- scripts/install_bot_dependencies.sh
+++ scripts/install_bot_dependencies.sh
@@ -2,7 +2,7 @@
 sudo pip3 install virtualenv
 cd /home/ec2-user/app
 # sudo yum install jq (jq needs to be installed)
-key=$(jq .BOT_KEY ../creds/creds.json) #retrieve key
+key=$(jq .KEY ../creds/creds.json) #retrieve key
 virtualenv environment
 source environment/bin/activate
 echo KEY=$key > .env #inject key
